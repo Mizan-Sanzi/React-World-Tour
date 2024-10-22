b27ffd0b2c080f0c05f500bae27c68b8a9d2db5c
mizansanzi <pleasemailmizan@gmail.com>
2024-10-22 14:40:13 +0600

    tour continuous

---
+++ src/components/countries/country/country.jsx
@@ -0,0 +1,14 @@
+import './country.css'
+const Country = ({country}) => {
+    const {name, flags, area, population}=country;
+    return (
+        <div className='country'>
+            <h3>Name: {name?.common}</h3>
+            <img src={flags.png} alt="" />
+            <p>Area: {area} </p>
+            <p>Population: {population} </p>
+        </div>
+    );
+};
+
+export default Country;
